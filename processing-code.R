24ba3db569cba1a6edc543c936c65514be51ed87
Gede Primahadi Wijaya Rajeg <primahadiwijaya@gmail.com>
2025-01-29 11:34:13 +0800

    Update README and code; add LICENSE.

--- processing-code.R
+++ processing-code.R
@@ -1,9 +1,51 @@
 library(tidyverse)
 library(readxl)
+library(googledrive)
+library(googlesheets4)
+
 sheets <- paste("Sheet", 1:22, sep = "")
-files <- dir(pattern = ".xlsx")
+files <- dir(path = "data-source/", pattern = ".xlsx", full.names = TRUE)
 
-df <- map2(.x = sheets, .y = files, ~read_xlsx(path = .y, sheet = .x, col_names = c("Dutch_Orig", "English_gloss", "High_Language_HL", "Contemporary_Equiv_HL", "Low_Language_LL", "Contemporary_Equiv_LL", "Remarks")))
+df <- map2(.x = sheets, 
+           .y = files, 
+           ~read_xlsx(path = .y, 
+                      sheet = .x, 
+                      col_names = c("Dutch_Orig", "English_gloss", 
+                                    "High_Language_HL", "Contemporary_Equiv_HL", 
+                                    "Low_Language_LL", "Contemporary_Equiv_LL", 
+                                    "Remarks"))) |> 
+  map2(.y = 1:22,
+       ~mutate(.x, pagenum = .y))
 
+walland <- df |> 
+  list_rbind() |> 
+  mutate(across(where(is.character), ~replace_na(., "")))
 
+walland1 <- walland |> 
+  # replace \r\n
+  mutate(Dutch_Orig = str_replace_all(Dutch_Orig, "(?<!\\-)\\r\\n", " "),
+         Dutch_Orig = str_replace_all(Dutch_Orig, "(?<=\\-)\\r\\n", ""),
+         English_gloss = str_replace_all(English_gloss, "([[:punct:]])\\r\\n", "\\1"),
+         English_gloss = str_replace_all(English_gloss, "(?<![[:punct:]])\\r\\n", " "),
+         High_Language_HL = str_replace_all(High_Language_HL, "(?<=[[:punct:]])\\r\\n", ""),
+         High_Language_HL = str_replace_all(High_Language_HL, "(?<![[:punct:]])\\r\\n", " "),
+         Contemporary_Equiv_HL = str_replace_all(Contemporary_Equiv_HL, "(\\~)\\r\\n", "\\1"),
+         Contemporary_Equiv_HL = str_replace_all(Contemporary_Equiv_HL, "(\\!)\\r\\n", "\\1; "),
+         Low_Language_LL = str_replace_all(Low_Language_LL, "(?<=\\-)\\r\\n", "")) |> 
+  mutate(Dutch_Orig = str_replace_all(Dutch_Orig, "\\-\\s+", "-"),
+         English_gloss = str_replace_all(English_gloss, "\\-\\s+", "-"),
+         High_Language_HL = str_replace_all(High_Language_HL, "\\-\\s+", "-"),
+         Low_Language_LL = str_replace_all(Low_Language_LL, "\\-\\s+", "-"))
 
+# googledrive::drive_create(name = "Walland-1864-annotated",
+#                           path = "https://drive.google.com/drive/u/0/folders/1GEcnGtmDGHdJO2AH8wWqXTd_1aaNcE8W",
+#                           type = "spreadsheet")
+# Created Drive file:
+#   • Walland-1864-annotated <id: 1VCUJNGysXkmZ31eOTruHjfFYrp0_o5qCvOe3Ule67A4>
+#   With MIME type:
+#   • application/vnd.google-apps.spreadsheet
+
+googlesheets4::sheet_write(walland1, ss = "1VCUJNGysXkmZ31eOTruHjfFYrp0_o5qCvOe3Ule67A4", sheet = "main")
+write_csv(walland1, "data-output/walland-1864-annotated.csv")
+write_tsv(walland1, "data-output/walland-1864-annotated.tsv")
+write_rds(walland1, "data-output/walland-1864-annotated.rds")
---
+++ terminal-history.sh
@@ -0,0 +1,8 @@
+#GRajeg@DESKTOP-H1BVLT0 MINGW64 /c/Users/GRajeg/OneDrive - Nexus365/Documents/Research/walland-1864-annotated (mch/walland-1864-annotated (main)
+$ mkdir data-source
+
+#GRajeg@DESKTOP-H1BVLT0 MINGW64 /c/Users/GRajeg/OneDrive - Nexus365/Documents/Research/walland-1864-annotated (main)
+$ mv Annotated\ Old\ Enggano\ list\ from\ “Het\ Eiland\ Engano”\ \(Walland\ 1864\)* data-source
+
+#GRajeg@DESKTOP-H1BVLT0 MINGW64 /c/Users/GRajeg/OneDrive - Nexus365/Documents/Research/walland-1864-annotated (main)
+$ mkdir data-output
